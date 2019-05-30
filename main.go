0f9dfece19a29d08ef3bea455fbe729ff1069047
Lluc Martorell <superpesi+git@gmail.com>
2019-05-30 12:06:27 +0200

    Dockerized server can load multiple level of folders inside buckets

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"path"
 
@@ -31,7 +32,48 @@ func getEnv(env string, defaultValue string) string {
 	return defaultValue
 }
 
-// TODO: Needs to be recursive
+func getObjectsFromPath(
+	bucketName string, objectsPath string, name string,
+) []fakestorage.Object {
+	files, err := ioutil.ReadDir(objectsPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	objects := []fakestorage.Object{}
+	for _, file := range files {
+		unescapedName, _ := url.PathUnescape(file.Name())
+
+		objectName := path.Join(name, unescapedName)
+		objectPath := path.Join(objectsPath, unescapedName)
+
+		if file.IsDir() {
+			objects = append(
+				objects,
+				getObjectsFromPath(bucketName, objectPath, objectName)...,
+			)
+			continue
+		}
+
+		fileContent, err := ioutil.ReadFile(objectPath)
+
+		if err != nil {
+			panic(err)
+		}
+
+		object := fakestorage.Object{
+			BucketName: bucketName,
+			Name:       objectName,
+			Content:    fileContent,
+		}
+
+		objects = append(objects, object)
+	}
+
+	return objects
+}
+
 // The buckets are the first level of folders...
 // Then are part of that bucket but needs to be loaded
 func generateObjectsFromFiles() []fakestorage.Object {
@@ -39,57 +81,23 @@ func generateObjectsFromFiles() []fakestorage.Object {
 
 	// if a storage volume is mounted in the container
 	if _, err := os.Stat(rootDir); !os.IsNotExist(err) {
-		// list the content
-		files, err := ioutil.ReadDir(rootDir)
+		buckets, err := ioutil.ReadDir(rootDir)
 
 		if err != nil {
 			panic(err)
 		}
 
-		for _, file := range files {
-			filePath := path.Join(rootDir, file.Name())
-			fileInfo, err := os.Stat(filePath)
+		for _, bucket := range buckets {
+			unescapedBucketName, _ := url.PathUnescape(bucket.Name())
+			bucketPath := path.Join(rootDir, unescapedBucketName)
 
-			if err != nil {
-				panic(err)
-			}
-
-			// if it's a directory, it look for the files it's containing
-			if fileInfo.IsDir() {
-				bucket := file
-				bucketPath := filePath
-
-				bucketFiles, err := ioutil.ReadDir(bucketPath)
-
-				if err != nil {
-					panic(err)
-				}
+			if bucket.IsDir() {
+				objects = append(
+					objects, getObjectsFromPath(unescapedBucketName, bucketPath, "")...,
+				)
 
 				fmt.Printf("Found bucket `%s`\n", bucket.Name())
-
-				// for each file create an object
-				for _, file := range bucketFiles {
-					filePath := path.Join(bucketPath, file.Name())
-					fileContent, err := ioutil.ReadFile(filePath)
-
-					if err != nil {
-						panic(err)
-					}
-
-					fmt.Printf(
-						"Creating object `%s` in bucket `%s`\n",
-						file.Name(),
-						bucket.Name(),
-					)
-
-					object := fakestorage.Object{
-						BucketName: bucket.Name(),
-						Name:       file.Name(),
-						Content:    fileContent,
-					}
-
-					objects = append(objects, object)
-				}
+				fmt.Printf("Add %d file/s in `%s`\n", len(objects), bucket.Name())
 			}
 		}
 	}
